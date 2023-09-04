02e2741342ee7dbbfac8b5fea1f50b22d5e06722
AmanGH009 <aman.mandal.71334@gmail.com>
2023-09-04 11:32:30 +0545

    Done with home page animation

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+function firstPageAim() {
+    var tl = gsap.timeline();
+
+    tl.from("#nav", {
+        y: 10,
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    }).to(".boundingelem", {
+        y: 0,
+        duration: 2,
+        ease: Expo.easeInOut,
+        stagger: .2,
+        delay: -1.5
+    }).from("#herofooter", {
+        opacity: 0,
+        duration: 1.5,
+        delay: -1.4,
+        ease: Expo.easeInOut,
+    })
+}
+
 function circleMouseFoller() {
     window.addEventListener("mousemove", function (dets) {
         document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`
@@ -10,3 +32,4 @@ function circleMouseFoller() {
 }
 
 circleMouseFoller();
+firstPageAim();
